71dce82aa6db06e02ffb25500e2e43caac265919
Ramon Barbosa <ramonflmr@gmail.com>
2020-08-20 23:24:37 -0300

    Add success test

--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -5,7 +5,7 @@ from app.context import AppContext
 from flask import json
 
 
-class TestTransactions(unittest.TestCase):
+class TestRegisterTransaction(unittest.TestCase):
     def setUp(self):
         app = create_app('config.TestConfig')
         self.client = app.test_client()
@@ -16,14 +16,14 @@ class TestTransactions(unittest.TestCase):
         self.context.StoreRepository.delete_all()
 
 
-    def _add_store(self):
+    def _add_sample_store(self):
         store = Store("45283163000167", "Nosso Restaurante de Todo Dia LTDA", "Fabio I.", "11909000300")
         self.context.StoreRepository.add(store)
 
     def test_unprocessable_if_invalid_store(self):
         headers = {'content-type': 'application/json'}
         result = self.client.post('/v1/transacao/', headers=headers,
-                                  data=json.dumps({"estabelecimento": "045.283.163/0001-67",
+                                  data=json.dumps({"estabelecimento": "60.283.163/0001-67",
                                                    "cliente" : "teste", "valor" : 10, "descricao" : "teste"}),
                                   follow_redirects=True)
         self.assertEqual(result.status_code, 422)
@@ -32,7 +32,18 @@ class TestTransactions(unittest.TestCase):
         headers = {'content-type': 'application/json'}
         result = self.client.post('/v1/transacao/', headers=headers,
                                   data=json.dumps({"estabelecimento": "45.283.163/0001-67",
-                                                   "cliente": "04003003543", "valor": 10.1, "descricao": "teste"}),
+                                                   "cliente": "594.214.930-01", "valor": 10.1, "descricao": "teste"}),
                                   follow_redirects=True)
 
         self.assertEqual(result.status_code, 422)
+
+    def test_ok_if_valid_client(self):
+        self._add_sample_store()
+        headers = {'content-type': 'application/json'}
+        result = self.client.post('/v1/transacao/', headers=headers,
+                                  data=json.dumps({"estabelecimento": "45.283.163/0001-67",
+                                                   "cliente": "094.214.930-01", "valor": 590.01,
+                                                   "descricao": "Almoço em restaurante chique pago via Shipay!"}),
+                                  follow_redirects=True)
+
+        self.assertEqual(result.status_code, 200)
